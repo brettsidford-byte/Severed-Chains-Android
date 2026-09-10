bf2d2015083117e818013706e1d110bf77331d84
brettsidford-byte <brettsidford@gmail.com>
2026-09-11 00:57:16 +0100

    Add Android build probe: android/app/build.gradle.kts

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "legend.severedchains.android"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "legend.severedchains.android"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0-android-probe"
+
+        ndk {
+            abiFilters += listOf("arm64-v8a")
+        }
+    }
+
+    buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}

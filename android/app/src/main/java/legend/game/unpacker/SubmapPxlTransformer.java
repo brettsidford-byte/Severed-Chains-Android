a1f6969df4dc3a2d105aed6fa85eb38af9da142c
brettsidford-byte <brettsidford@gmail.com>
2026-09-11 12:31:32 +0100

    android: add optional unpacker transformer boundary

---
+++ android/app/src/main/java/legend/game/unpacker/SubmapPxlTransformer.java
@@ -0,0 +1,8 @@
+package legend.game.unpacker;
+
+import java.util.Set;
+
+public final class SubmapPxlTransformer {
+  private SubmapPxlTransformer() { }
+  public static void transform(final PathNode root, final Transformations transformations, final Set<String> flags) { }
+}

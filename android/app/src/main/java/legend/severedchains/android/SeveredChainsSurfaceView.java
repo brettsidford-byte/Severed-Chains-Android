08df01c413d73a7d38c4ecda977688924cc1b7e7
brettsidford-byte <brettsidford@gmail.com>
2026-09-11 09:24:19 +0100

    Build Android storage and diagnostic milestone

--- android/app/src/main/java/legend/severedchains/android/SeveredChainsSurfaceView.java
+++ android/app/src/main/java/legend/severedchains/android/SeveredChainsSurfaceView.java
@@ -3,6 +3,7 @@ package legend.severedchains.android;
 import android.content.Context;
 import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
+import android.util.Log;
 import android.view.InputDevice;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
@@ -13,6 +14,7 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public final class SeveredChainsSurfaceView extends GLSurfaceView {
+    private static final String TAG = "SeveredChains";
     private final StatusRenderer renderer;
 
     public SeveredChainsSurfaceView(final Context context) {
@@ -27,13 +29,17 @@ public final class SeveredChainsSurfaceView extends GLSurfaceView {
 
     @Override
     public boolean onKeyDown(final int keyCode, final KeyEvent event) {
-        renderer.setLastInput(String.format(Locale.ROOT, "KEY %s", KeyEvent.keyCodeToString(keyCode)));
+        final String input = String.format(Locale.ROOT, "KEY %s", KeyEvent.keyCodeToString(keyCode));
+        Log.i(TAG, input);
+        renderer.setLastInput(input);
         return true;
     }
 
     @Override
     public boolean onKeyUp(final int keyCode, final KeyEvent event) {
-        renderer.setLastInput(String.format(Locale.ROOT, "KEY UP %s", KeyEvent.keyCodeToString(keyCode)));
+        final String input = String.format(Locale.ROOT, "KEY UP %s", KeyEvent.keyCodeToString(keyCode));
+        Log.i(TAG, input);
+        renderer.setLastInput(input);
         return true;
     }
 
@@ -42,7 +48,9 @@ public final class SeveredChainsSurfaceView extends GLSurfaceView {
         if ((event.getSource() & InputDevice.SOURCE_JOYSTICK) != 0) {
             final float x = event.getAxisValue(MotionEvent.AXIS_X);
             final float y = event.getAxisValue(MotionEvent.AXIS_Y);
-            renderer.setLastInput(String.format(Locale.ROOT, "STICK %.2f, %.2f", x, y));
+            final String input = String.format(Locale.ROOT, "STICK %.2f, %.2f", x, y);
+            Log.i(TAG, input);
+            renderer.setLastInput(input);
             return true;
         }
         return super.onGenericMotionEvent(event);
@@ -58,11 +66,13 @@ public final class SeveredChainsSurfaceView extends GLSurfaceView {
         @Override
         public void onSurfaceCreated(final GL10 gl, final EGLConfig config) {
             GLES30.glClearColor(0.02f, 0.02f, 0.02f, 1.0f);
+            Log.i(TAG, "GLES surface created: version=" + GLES30.glGetString(GLES30.GL_VERSION) + ", renderer=" + GLES30.glGetString(GLES30.GL_RENDERER));
         }
 
         @Override
         public void onSurfaceChanged(final GL10 gl, final int width, final int height) {
             GLES30.glViewport(0, 0, width, height);
+            Log.i(TAG, "GLES surface size=" + width + "x" + height);
         }
 
         @Override

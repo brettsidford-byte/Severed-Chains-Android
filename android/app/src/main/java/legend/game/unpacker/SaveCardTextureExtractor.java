b253385f4f37ab2666ed7b425e48ecbbbb7b3bbb
brettsidford-byte <brettsidford@gmail.com>
2026-09-11 12:31:27 +0100

    android: add optional unpacker transformer boundary

---
+++ android/app/src/main/java/legend/game/unpacker/SaveCardTextureExtractor.java
@@ -0,0 +1,9 @@
+package legend.game.unpacker;
+
+import java.util.Set;
+
+public final class SaveCardTextureExtractor {
+  private SaveCardTextureExtractor() { }
+  public static boolean portraitExtractorDiscriminator(final PathNode node, final Set<String> flags) { return false; }
+  public static void portraitExtractorTransformer(final PathNode node, final Transformations transformations, final Set<String> flags) { }
+}

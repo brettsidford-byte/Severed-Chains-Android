3dab45f5bbd2f57fa5a5ffc8e9292d7c56ac2c89
brettsidford-byte <brettsidford@gmail.com>
2026-09-11 12:31:29 +0100

    android: add optional unpacker transformer boundary

---
+++ android/app/src/main/java/legend/game/unpacker/CtmdTransformer.java
@@ -0,0 +1,9 @@
+package legend.game.unpacker;
+
+import java.util.Set;
+
+public final class CtmdTransformer {
+  private CtmdTransformer() { }
+  public static boolean ctmdDiscriminator(final PathNode node, final Set<String> flags) { return false; }
+  public static void ctmdTransformer(final PathNode node, final Transformations transformations, final Set<String> flags) { }
+}
